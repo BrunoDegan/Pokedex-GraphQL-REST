dcf973cfa712adb44576801f385da6092dc5548f
Bruno <bmdegan@gmail.com>
2025-04-22 02:47:10 -0300

    [IMPROVEMENT] - Adding new safeDrawing inset for app Scaffold

--- app/src/main/java/com/brunodegan/pokedex/base/PokedexMainActivity.kt
+++ app/src/main/java/com/brunodegan/pokedex/base/PokedexMainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -25,7 +27,10 @@ class PokeDexMainActivity : ComponentActivity() {
                 LocalMetrics
             }
             PokedexTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    contentWindowInsets = WindowInsets.safeDrawing,
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
                     Surface(modifier = Modifier.padding(innerPadding)) {
                         AppNavHost()
                     }
